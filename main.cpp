3c55635d4ed903ac54b7a4d1408cd9a55a38ca01
lmusr777 <102671015+lmusr777@users.noreply.github.com>
2026-09-16 14:18:36 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //декларації
+    float a, P;
+
+    //введення
+    cout << "a = ";
+    cin >> a;
+
+    //підрахунок
+    P = 4 * a;   //+ - * /
+
+    //виведення
+    cout << "P = " << P << endl;
+
+    //system("pause");
+    cout << "...";
+    cin.get();
+    cin.get();
+
+    return 0;
+}
